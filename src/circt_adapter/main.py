01fb117f8cedc819d5a330d02c994d1dc9f6b3d3
Calin Novo <calin_novo@hotmail.com>
2025-02-11 22:35:32 +0000

    Done some work on main.py

--- src/circt_adapter/main.py
+++ src/circt_adapter/main.py
@@ -1,13 +1,15 @@
 # main.py
-
+import json
 from typing import Dict
 from model_explorer import *
+from model_explorer.utils import convert_builtin_resp
+from model_explorer.types import GraphCollection, ModelExplorerGraphs
 
 class MyAdapter(Adapter):
 
   metadata = AdapterMetadata(id='circt_adapter',
                              name='CIRCT Adapter',
-                             description='Model Explorer Adapter for CIRCT generated JSON and MLIR files."',
+                             description='Model Explorer Adapter for CIRCT generated JSON and MLIR files.',
                              source_repo='https://github.com/GroupTango/circt-adapter/tree/main',
                              fileExts=['mlir', 'json'])
 
@@ -24,46 +26,14 @@ class MyAdapter(Adapter):
     if (model_path.endswith(".mlir")):
       src_string = ConvertCirctToJson(src_string)
     
-    return {'graphs': ConvertJsonToGraphs(src_string)}
+    return {'graphCollections': ConvertJsonToGraphs(src_string)}
 
 def ConvertCirctToJson(mlir_str: str) -> str:
   #Add C++ call here
   return ""
 
 def ConvertJsonToGraphs(json_str: str):
-  #Temporarily return hardcoded graph, based on example mlir file
-  graph1 = graph_builder.Graph(id='test_mlir_file')
-  
-  moduleA1 = graph_builder.GraphNode(id='ia1', label='input', namespace='Module A')
-  combA1 = graph_builder.GraphNode(id='ca1', label='comb', namespace='Module A')
-  seqA1 = graph_builder.GraphNode(id='sa1', label='seq', namespace='Module A')
-  outputA1 = graph_builder.GraphNode(id='oa1', label='output', namespace='Module A')
-
-  graph1.nodes.extend([moduleA1, combA1, seqA1, outputA1])
-
-  combA1.incomingEdges.append(graph_builder.IncomingEdge(sourceNodeId='ia1'))
-  seqA1.incomingEdges.append(graph_builder.IncomingEdge(sourceNodeId='ca1'))
-  outputA1.incomingEdges.append(graph_builder.IncomingEdge(sourceNodeId='sa1'))
-
-  moduleA1.attrs.append(graph_builder.KeyValue(key="type", value="hw.module"))
-  moduleA1.attrs.append(graph_builder.KeyValue(key="in parameters", value="%clk: !seq.clock, %in0 : i32, %in1 : i32"))
-  moduleA1.attrs.append(graph_builder.KeyValue(key="out parameters", value="out0 : i32"))
-  
-  combA1.attrs.append(graph_builder.KeyValue(key="type", value="comb.add"))
-  combA1.attrs.append(graph_builder.KeyValue(key="operands", value="%in0, %in1"))
-  combA1.attrs.append(graph_builder.KeyValue(key="results", value="%0 = i32"))
-  combA1.style = graph_builder.backgroundColor = "yellow"
-  
-  seqA1.attrs.append(graph_builder.KeyValue(key="type", value="seq.compreg"))
-  seqA1.attrs.append(graph_builder.KeyValue(key="operands", value="%0, %clk"))
-  seqA1.attrs.append(graph_builder.KeyValue(key="results", value="%1 = i32"))
-  seqA1.style = graph_builder.backgroundColor = "green"
-    
-  outputA1.attrs.append(graph_builder.KeyValue(key="type", value="hw.output"))
-  outputA1.attrs.append(graph_builder.KeyValue(key="operands", value="%1"))
-
-  return [graph1]
+  return convert_builtin_resp(json_str)
 
 def CloneModule(src_node : graph_builder.GraphNode) -> graph_builder.GraphNode:
   return None
-  
