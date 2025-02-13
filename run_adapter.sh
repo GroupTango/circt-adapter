43e43a7f1aded4fb30fa0ed8e8a371af10bc4e5f
Kacper Michalik <km924@cam.ac.uk>
2025-02-13 07:23:34 +0000

    Minor change to adapter run scipt. Added more example files.

--- run_adapter.sh
+++ run_adapter.sh
@@ -2,4 +2,4 @@
 python3 -m venv venv
 source $PWD/venv/bin/activate
 pip install ai-edge-model-explorer
-model-explorer $PWD/examples/visualize-example.mlir --extensions=circt_adapter
+model-explorer --extensions=circt_adapter
